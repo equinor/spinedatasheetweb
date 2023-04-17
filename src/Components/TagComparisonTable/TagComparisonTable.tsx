4fdd9b5c2b7d3dc0c8e9e8572645b7aa3b71d9a4
Arian <89781510+arian-garshi@users.noreply.github.com>
2023-04-17 10:23:31 +0200

    feat: columns and rows now all white

--- src/Components/TagComparisonTable/TagComparisonTable.tsx
+++ src/Components/TagComparisonTable/TagComparisonTable.tsx
@@ -28,7 +28,7 @@ function TagComparisonTable({
 
     return (
         <>
-            <div className="ag-theme-alpine" style={{ flex: "1 1 auto", width: "90%" }}>
+            <div className="ag-theme-alpine" style={{ flex: "1 1 auto", width: "100%" }}>
                 <AgGridReact
                     rowData={tagRows}
                     columnDefs={newColumns}
--- src/Views/EquipmentListView.tsx
+++ src/Views/EquipmentListView.tsx
@@ -1,14 +1,12 @@
-import { Progress, Tabs } from "@equinor/eds-core-react"
-import { useCurrentContext } from "@equinor/fusion"
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import styled from "styled-components"
-import { GetDatasheetService } from "../api/DatasheetService"
-import TotalReviewStatus from "../Components/Charts/TotalReviewStatus"
-import TotalTagRequirement from "../Components/Charts/TotalTagRequirement"
-import EquipmentListTable from "../Components/EquipmentListTable"
-import { Datasheet } from "../Models/Datasheet"
-import TagComparisonTable from "../Components/TagComparisonTable/TagComparisonTable"
+import { Progress, Tabs } from "@equinor/eds-core-react";
+import { useCurrentContext } from "@equinor/fusion";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { GetDatasheetService } from "../api/DatasheetService";
+import EquipmentListTable from "../Components/EquipmentListTable";
+import { Datasheet } from "../Models/Datasheet";
+import TagComparisonTable from "../Components/TagComparisonTable/TagComparisonTable";
 
 const Wrapper = styled.div`
     width: 100%;
@@ -17,61 +15,56 @@ const Wrapper = styled.div`
     align-items: center;
     flex-direction: column;
     padding: 20px 0;
-`
+`;
 
-const WrapperColumn = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    padding: 20px 0;
-    justify-content: center;
-    align-items: center;
-`
-
-const { Panel } = Tabs
-const { List, Tab, Panels } = Tabs
+const { Panel } = Tabs;
+const { List, Tab, Panels } = Tabs;
 
 const StyledTabPanel = styled(Panel)`
-    padding-top: 0px;
+    padding-top: 0;
     border-top: 1px solid LightGray;
-`
+`;
 
 function EquipmentListView() {
-    const [activeTab, setActiveTab] = useState(0)
+    const [activeTab, setActiveTab] = useState(0);
 
-    const [tags, setTags] = useState<Datasheet[]>([])
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [error, setError] = useState<boolean>(false)
+    const [tags, setTags] = useState<Datasheet[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false);
 
-    const { projectId } = useParams<Record<string, string | undefined>>()
+    const { projectId } = useParams<Record<string, string | undefined>>();
 
-    const currentProject = useCurrentContext()
+    const currentProject = useCurrentContext();
 
     useEffect(() => {
         (async () => {
-            setError(false)
-            setIsLoading(false)
+            setError(false);
+            setIsLoading(false);
             if (currentProject !== null && currentProject.externalId !== null) {
                 try {
-                    setIsLoading(true)
-                    const datasheets: Datasheet[] = await (await GetDatasheetService())
-                        .getDatasheets()
-                    setTags(datasheets)
-                    setIsLoading(false)
+                    setIsLoading(true);
+                    const datasheets: Datasheet[] = await (
+                        await GetDatasheetService()
+                    ).getDatasheets();
+                    setTags(datasheets);
+                    setIsLoading(false);
                 } catch {
-                    console.error("Error loading tags")
-                    setError(true)
+                    console.error("Error loading tags");
+                    setError(true);
                 }
             }
-        })()
-    }, [currentProject, projectId])
+        })();
+    }, [currentProject, projectId]);
 
-    if (currentProject === null && (projectId === null || projectId === undefined)) {
-        return <div>No project selected</div>
+    if (
+        currentProject === null &&
+        (projectId === null || projectId === undefined)
+    ) {
+        return <div>No project selected</div>;
     }
 
     if (error) {
-        return <div>Error loading tags</div>
+        return <div>Error loading tags</div>;
     }
 
     if (isLoading) {
@@ -80,20 +73,20 @@ function EquipmentListView() {
                 <Progress.Circular size={16} color="primary" />
                 <div>Loading tags...</div>
             </>
-        )
+        );
     }
 
     if (tags.length === 0) {
-        return <div>No tags found for this project</div>
+        return <div>No tags found for this project</div>;
     }
 
     return (
         <Wrapper>
-            <WrapperColumn>
-                <TotalTagRequirement></TotalTagRequirement>
-                <TotalReviewStatus></TotalReviewStatus>
-            </WrapperColumn>
-            <Tabs style={{ width: "100%" }} activeTab={activeTab} onChange={setActiveTab}>
+            <Tabs
+                style={{ width: "100%" }}
+                activeTab={activeTab}
+                onChange={setActiveTab}
+            >
                 <List>
                     <Tab>Tag info</Tab>
                     <Tab>Tag comparison</Tab>
@@ -106,8 +99,8 @@ function EquipmentListView() {
                         <TagComparisonTable tags={tags} />
                     </StyledTabPanel>
                 </Panels>
-            </Tabs >
+            </Tabs>
         </Wrapper>
-    )
+    );
 }
-export default EquipmentListView
+export default EquipmentListView;
