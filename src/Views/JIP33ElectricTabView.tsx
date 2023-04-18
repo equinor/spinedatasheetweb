4a661c38d66029d184727e7ceb2fb07ddbcf81fc
Daniel Böhme <dboh@equinor.com>
2023-04-18 22:28:15 +0200

    Removed copied instrument row data
    Had moved instrument rowdata, but somehow it had copied so removing old files

--- src/Views/JIP33ElectricTabView.tsx
+++ src/Views/JIP33ElectricTabView.tsx
@@ -1,8 +1,7 @@
 import { generalRowData } from "../Components/JIP33Table/RowData/Electric/GeneralRowData"
-import { Tabs, Typography } from "@equinor/eds-core-react"
+import { Typography } from "@equinor/eds-core-react"
 import styled from "styled-components"
 import { useState } from "react"
-import JIP33Table from "../Components/JIP33Table/JIP33Table"
 import { BackButton } from "../Components/BackButton"
 import { useParams } from "react-router-dom"
 import { Datasheet } from "../Models/Datasheet"
@@ -27,26 +26,23 @@ import { testingAndInspectionRowData } from "../Components/JIP33Table/RowData/El
 import { preservationAndStorageRowData } from "../Components/JIP33Table/RowData/Electric/PreservationAndStorageRowData"
 import { documentationRowData } from "../Components/JIP33Table/RowData/Electric/DocumentationRowData"
 import { informationRowData } from "../Components/JIP33Table/RowData/Electric/InformationRowData"
+import SideMenu from "../Components/SideMenu"
 
-const WrapperTabs = styled.div`
-    width: 100%;
-    display: flex;
-    float: left;
-    flex-direction: column;
+const TopBar = styled.div`
+    padding-top: 0;
+    border-bottom: 1px solid LightGray;
+    z-index: 100;
+    padding-top: 20px;
+    // position: sticky;
+    // top: 0;
 `
 
-const { Panel } = Tabs
-const { List, Tab, Panels } = Tabs
-
-const StyledTabPanel = styled(Panel)`
-    padding-top: 0;
-    border-top: 1px solid LightGray;
-    flexWrap: "wrap"
+const Body = styled.div`
+    height: 92%; //92
 `
 
 function JIP33ElectricTabView({
 }) {
-    const [activeTab, setActiveTab] = useState(0)
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [error, setError] = useState<boolean>(false)
 
@@ -86,109 +82,36 @@ function JIP33ElectricTabView({
     //     return <div>No tag selected</div>
     // }
 
+    const sideMenuList = [
+        "Information", "Purchaser information", "General",
+        "Electrical operating conditions", "Rating", "ASD fed motor data",
+        "Site conditions / location environment", "Starting performance",
+        "Operating performance", "Noise", "Motor construction", "Fan",
+        "Main terminal box", "Bearings", "Space heaters", "Mounting",
+        "Instrumentation", "Surface protection", "Motors for hazardous locations",
+        "Testing and inspection", "Preservation and storage", "Documentation"
+    ]
+
+    const rowDataList = [
+        informationRowData, purchaserInformationRowData, generalRowData,
+        electricalOperatingConditionsRowData, ratingRowData, aSDFedMotorDataRowData,
+        siteConditionsLocationEnvironmentRowData, startingPerformanceRowData,
+        operatingPerformanceRowData, noiseRowData, motorConstructionRowData, fanRowData,
+        mailTerminalBoxRowData, bearingsRowData, spaceHeatersRowData, mountingRowData,
+        instrumentationRowData, surfaceProtectionRowData, motorsForHazardousLocationsRowData,
+        testingAndInspectionRowData, preservationAndStorageRowData, documentationRowData
+    ]
+
     return (
-        <>
-            <WrapperTabs>
+        <Body>
+            <TopBar>
                 <Typography variant="h3">
                     <BackButton />
                     JIP33 table
                 </Typography>
-                <Tabs activeTab={activeTab} onChange={setActiveTab} scrollable>
-                    <List>
-                        <Tab>Information</Tab>
-                        <Tab>Purchaser information</Tab>
-                        <Tab>General</Tab>
-                        <Tab>Electrical operating conditions</Tab>
-                        <Tab>Rating</Tab>
-                        <Tab>ASD fed motor data</Tab>
-                        <Tab>Site conditions / location environment</Tab>
-                        <Tab>Starting performance</Tab>
-                        <Tab>Operating performance</Tab>
-                        <Tab>Noise</Tab>
-                        <Tab>Motor construction</Tab>
-                        <Tab>Fan</Tab>
-                        <Tab>Main terminal box</Tab>
-                        <Tab>Bearings</Tab>
-                        <Tab>Space heaters</Tab>
-                        <Tab>Mounting</Tab>
-                        <Tab>Instrumentation</Tab>
-                        <Tab>Surface protection</Tab>
-                        <Tab>Motors for hazardous locations</Tab>
-                        <Tab>Testing and inspection</Tab>
-                        <Tab>Preservation and storage</Tab>
-                        <Tab>Documentation</Tab>
-                    </List>
-                    <Panels>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={informationRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={purchaserInformationRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={generalRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={electricalOperatingConditionsRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={ratingRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={aSDFedMotorDataRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={siteConditionsLocationEnvironmentRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={startingPerformanceRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={operatingPerformanceRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={noiseRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={motorConstructionRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={fanRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={mailTerminalBoxRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={bearingsRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={spaceHeatersRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={mountingRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={instrumentationRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={surfaceProtectionRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={motorsForHazardousLocationsRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={testingAndInspectionRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={preservationAndStorageRowData} />
-                        </StyledTabPanel>
-                        <StyledTabPanel>
-                            <JIP33Table rowData={documentationRowData} />
-                        </StyledTabPanel>
-                    </Panels>
-                </Tabs >
-            </WrapperTabs >
-        </>
+            </TopBar>
+            <SideMenu sideMenuList={sideMenuList} rowDataList={rowDataList} />
+        </Body>
     )
 }
 
