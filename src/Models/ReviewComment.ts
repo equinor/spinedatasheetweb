dadaa97400c4c1280c70ac3a81b3fa1bbefd810e
Fredrik <flek@equinor.com>
2023-08-07 14:24:05 +0200

    remove redundant semicolons

--- src/Models/ReviewComment.ts
+++ src/Models/ReviewComment.ts
@@ -2,18 +2,18 @@ import { RevisionContainerReview } from "./RevisionContainerReview"
 import { TagDataReview } from "./TagDataReview"
 
 export class ReviewComment implements Components.Schemas.CommentDto {
-    id?: string; // uuid
-    createdDate?: string; // date-time
-    modifiedDate?: string; // date-time
-    userId?: string; // uuid
-    commenterName?: string | null;
-    lastEdited?: string | null; // date-time
-    isEdited?: boolean;
-    text?: string | null;
-    property?: string | null;
-    commentLevel?: Components.Schemas.CommentLevel /* int32 */;
-    tagDataReviewId?: string | null; // uuid
-    revisionContainerReviewId?: string | null; // uuid
+    id?: string // uuid
+    createdDate?: string // date-time
+    modifiedDate?: string // date-time
+    userId?: string // uuid
+    commenterName?: string | null
+    lastEdited?: string | null // date-time
+    isEdited?: boolean
+    text?: string | null
+    property?: string | null
+    commentLevel?: Components.Schemas.CommentLevel /* int32 */
+    tagDataReviewId?: string | null // uuid
+    revisionContainerReviewId?: string | null // uuid
 
     constructor(init?: Partial<ReviewComment>) {
         Object.assign(this, init)
