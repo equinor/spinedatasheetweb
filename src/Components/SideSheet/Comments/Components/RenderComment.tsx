f58da98b6b26324fe4867bba24ba2cb29c424f8a
Fredrik <flek@equinor.com>
2023-09-04 10:28:31 +0200

    Set comment options on hover and display message deleted

--- src/Components/SideSheet/Comments/Components/RenderComment.tsx
+++ src/Components/SideSheet/Comments/Components/RenderComment.tsx
@@ -1,16 +1,22 @@
 import React, {
-    Dispatch, FC, SetStateAction, useState,
+    Dispatch, FC, SetStateAction, useRef, useState,
 } from "react"
 import { styled } from "styled-components"
-import { Button, Input, Typography } from "@equinor/eds-core-react"
+import {
+    Button, Icon, Input, Popover, Typography,
+} from "@equinor/eds-core-react"
+import {
+    delete_to_trash, edit,
+} from "@equinor/eds-icons"
 import { ReviewComment } from "../../../../Models/ReviewComment"
 import { GetCommentService } from "../../../../api/CommentService"
 
-const SubmitEditButton = styled(Button)`
-    margin-right: 15px;
-`
 const CommentText = styled(Typography)`
     margin: 10px 0;
+`
+
+const SubmitEditButton = styled(Button)`
+    margin-right: 15px;
 `
 
 interface RenderCommentProps {
@@ -19,6 +25,7 @@ interface RenderCommentProps {
     setUpdateMode: any,
     reviewComments: ReviewComment[],
     setReviewComments: Dispatch<SetStateAction<ReviewComment[]>>
+    isCurrentUser: boolean
 }
 
 const formatDate = (dateString: string | null | undefined) => {
@@ -33,6 +40,7 @@ const formatDate = (dateString: string | null | undefined) => {
 }
 
 const updateComment = async (
+    softDelete: boolean,
     newCommentText: string,
     comment: ReviewComment,
     reviewComments: ReviewComment[],
@@ -43,6 +51,7 @@ const updateComment = async (
             const commentService = await GetCommentService()
             const newComment = { ...comment }
             newComment.text = newCommentText
+            newComment.softDeleted = softDelete
             const updatedComment = await commentService.updateComment(comment.id, newComment)
             const newReviewComments = reviewComments.map((c) => (c.id !== comment.id ? c : updatedComment))
             setReviewComments(newReviewComments)
@@ -52,16 +61,65 @@ const updateComment = async (
     }
 }
 
+const deleteComment = async (
+    softDelete: boolean,
+    comment: ReviewComment,
+    reviewComments: ReviewComment[],
+    setReviewComments: Dispatch<SetStateAction<ReviewComment[]>>,
+) => {
+    if (comment.id && comment.text) {
+        if (softDelete) {
+            updateComment(true, comment.text, comment, reviewComments, setReviewComments)
+        } else {
+            try {
+                const service = await GetCommentService()
+                await service.deleteComment(comment.id)
+                const newReviewComments = reviewComments.filter((c) => (c.id !== comment.id))
+                setReviewComments(newReviewComments)
+            } catch (error) {
+                console.error(`Error deleting comment: ${error}`)
+            }
+        }
+    }
+}
+
 const RenderComment: FC<RenderCommentProps> = ({
-    comment, isUpdateMode, setUpdateMode, reviewComments, setReviewComments,
+    comment, isUpdateMode, setUpdateMode, reviewComments, setReviewComments, isCurrentUser,
 }) => {
     const [editedComment, setEditedComment] = useState(comment.text || "")
+    const [open, setOpen] = useState(false)
 
     const editComment = (e: React.ChangeEvent<HTMLTextAreaElement>) => setEditedComment(e.target.value)
     const cancelEdit = () => setUpdateMode(false)
     const saveComment = () => {
-        updateComment(editedComment, comment, reviewComments, setReviewComments)
+        updateComment(false, editedComment, comment, reviewComments, setReviewComments)
         cancelEdit()
+    }
+
+    const anchorRef = useRef<HTMLParagraphElement>(null)
+
+    let timer: ReturnType<typeof setTimeout> | null = null
+    const openPopover = () => setOpen(true)
+    const closePopover = () => setOpen(false)
+    const handleHover = () => {
+        if (timer) {
+            clearTimeout(timer)
+        }
+        if (!isCurrentUser || comment.softDeleted) {
+            return
+        }
+        timer = setTimeout(() => {
+            openPopover()
+        }, 100)
+    }
+
+    const handleClose = () => {
+        if (timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+            closePopover()
+        }, 100)
     }
 
     if (isUpdateMode) {
@@ -79,9 +137,44 @@ const RenderComment: FC<RenderCommentProps> = ({
         )
     }
     return (
-        <CommentText>
-            {comment.text}
-        </CommentText>
+        <div>
+            <CommentText
+                aria-expanded={open}
+                ref={anchorRef}
+                onMouseOver={handleHover}
+                onFocus={handleHover}
+                onBlur={handleClose}
+                onMouseOut={handleClose}
+            >
+                {comment.text}
+            </CommentText>
+            <Popover
+                anchorEl={anchorRef.current}
+                onClose={handleClose}
+                open={open}
+                placement="top"
+                withinPortal
+                onMouseOver={handleHover}
+                onMouseLeave={handleClose}
+            >
+                <Popover.Header>
+                    <Button
+                        variant="ghost_icon"
+                        onClick={() => setUpdateMode((prevMode: boolean) => !prevMode)}
+                        title="Edit comment"
+                    >
+                        <Icon data={edit} size={16} color="#007079" />
+                    </Button>
+                    <Button
+                        variant="ghost_icon"
+                        onClick={() => deleteComment(true, comment, reviewComments, setReviewComments)}
+                        title="Delete"
+                    >
+                        <Icon data={delete_to_trash} size={16} color="#007079" />
+                    </Button>
+                </Popover.Header>
+            </Popover>
+        </div>
     )
 }
 
