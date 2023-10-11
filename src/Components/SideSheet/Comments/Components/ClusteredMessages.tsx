6b29d39eb0e1d7bbfcb653e16c3dc01eace973e2
Fredrik <flek@equinor.com>
2023-10-11 09:18:51 +0200

    wip

--- src/Components/SideSheet/Comments/Components/ClusteredMessages.tsx
+++ src/Components/SideSheet/Comments/Components/ClusteredMessages.tsx
@@ -123,6 +123,16 @@ const ClusteredMessages: FC<ClusteredMessagesProps> = ({ initEditMode, editMode 
         return restMessages
     }
 
+    const renderPhoto = (userId: string) => {
+        console.log(userId)
+        return (
+            <PersonPhoto
+                personId={userId}
+                size="large"
+            />
+        )
+    }
+
     return (
         <>
             {generateMessageCluster(activeConversation.messages).map((cluster, index) => (
@@ -140,13 +150,9 @@ const ClusteredMessages: FC<ClusteredMessagesProps> = ({ initEditMode, editMode 
                         <MessageContainer>
                             {/* TODO: change to PersonAvatar when docs are better */}
                             <PhotoContainer isCurrentUser={isCurrentUser(cluster.userId)}>
-                                {!isCurrentUser && (
-                                    <PersonPhoto
-                                        personId={cluster.userId}
-                                        key={`${cluster.userId}-${index}`}
-                                        size="large"
-                                    />
-                                )}
+                                {/* {!isCurrentUser && ( */}
+                                {renderPhoto(cluster.userId)}
+                                {/* )} */}
                                 <MessageBox
                                     key={`${cluster.userId}-${index}-${0}`}
                                     messageObject={cluster.messages[0]}
